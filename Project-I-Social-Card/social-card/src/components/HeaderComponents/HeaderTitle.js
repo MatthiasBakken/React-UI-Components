d965d205e4a06bed2c7f0453d29b93620b39176e
Matthias Bakken <matthias.bakken@gmail.com>
2019-03-03 13:12:00 +0700

    fixed link in title

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -9,7 +9,7 @@ const fullDate = today.toDateString();
 const HeaderTitle = () => {
   return (
     <div className={`${STYLE_BASE}titleDiv`} >
-      Lambda School <span className={`${STYLE_BASE}lambdaLink`} ><a href='#'>@LambdaSchool</a> {fullDate}</span>
+      Lambda School <span className={`${STYLE_BASE}lambdaLink`} ><a href='https://lambdaschool.com/'>@LambdaSchool</a> {fullDate}</span>
     </div>
   )
 };
